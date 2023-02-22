8e106b02ba8c7dc5707d61f5e3c010dabd55b62a
Kent Phan <kentphan2000@gmail.com>
2023-02-22 14:28:01 -0800

    test

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -23,10 +23,10 @@ export const serverSchema = z.object({
   DISCORD_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
-  AWS_REGION: z.string(),
+  AWS_REGION_TEST: z.string(),
   AWS_BUCKET_NAME: z.string(),
-  AWS_ACCESS_KEY_ID: z.string(),
-  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_ACCESS_KEY_ID_TEST: z.string(),
+  AWS_SECRET_ACCESS_KEY_TEST: z.string(),
 });
 
 /**
--- src/server/s3.ts
+++ src/server/s3.ts
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 import { env } from "../env/server.mjs";
 
 const s3 = new AWS.S3({
-  accessKeyId: env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-  region: env.AWS_REGION,
+  accessKeyId: env.AWS_ACCESS_KEY_ID_TEST,
+  secretAccessKey: env.AWS_SECRET_ACCESS_KEY_TEST,
+  region: env.AWS_REGION_TEST,
 });
 export default s3;
